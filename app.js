43964ba17e26d5b3a1f7cc3cf783a32b0e9936b2
Meetkalani <meetkalani2002@gmail.com>
2026-07-14 21:48:02 +0530

    chore: Set up middleware for JSON parsing, CORS, security, and logging

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// for logging the requests
+app.use(morgan("dev"));
 
 export default app;
